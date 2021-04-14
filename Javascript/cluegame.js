6589c928f1b29163605e3d82bb070317278828aa
khalilsliti <76879136+khalilsliti@users.noreply.github.com>
2021-04-14 18:26:55 +0200

    Add files via upload

---
+++ Javascript/cluegame.js
@@ -0,0 +1,18 @@
+var game = {};
+game.murderer = "??";
+game['weapons'] = [
+{type: 'lasers', location:'lab'},
+{type:'angry cats',location:'garden'},
+{type:'dish soap',location:'kitchen'},
+];
+game.name=[];
+game.name[0]='Mrs white';
+game.name.push('Mrs Green');
+ [game['first'],game['second']]=[true,false];
+ console.log(game.first)
+
+const {first,second}={second:1,first:2}
+console.log(first,second);
+console.log(typeof(first));
+
+
