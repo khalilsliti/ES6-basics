a65e45f0673f61341339d144ac78f4eaf77a227e
khalilsliti <76879136+khalilsliti@users.noreply.github.com>
2021-04-15 02:16:19 +0200

    Add files via upload

--- Javascript/challenge.js
+++ Javascript/challenge.js
@@ -1,3 +1,12 @@
-var p = {"name":"rusty","room":"kitchen","weapon":"candlestick"}
+var p = {"name":"rusty","room":"kitchen","weapon":"candlestick","arrayis":["a","b"]}
 let {weapon:x,room:y}=p;
 console.log(x,y)
+for (let key in p)
+{   if (typeof(p[key])=="object")
+    {console.log("arrray is ",p[key])}
+   else {
+    console.log(typeof(p[key]),"primitive value :",key,' :',p[key]);
+   }
+    
+    
+}
