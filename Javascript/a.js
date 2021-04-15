62d5e32307526861fbefb0839f7fa95e6c414d3a
khalilsliti <76879136+khalilsliti@users.noreply.github.com>
2021-04-15 15:54:35 +0200

    Add files via upload

---
+++ Javascript/a.js
@@ -0,0 +1,22 @@
+function createsuspect(name)
+{
+    return {
+        name: name,
+        color: name.split(' ')[1],
+        speak() {console.log("my name is ", name);}
+    }
+}
+var suspects = ["scarlet alo","dele3 bye ","bati5 hey"]
+var suspectsList = suspects.map(createsuspect)
+suspectsList.forEach(function (value) {value.speak()})
+
+
+const _ = {}
+_.map = function (list,callback)
+{
+    var storage = [];
+    _.each(list,function(v,i,list){
+        storage.push(callback(v,i,list))
+    })
+    return storage;
+}
